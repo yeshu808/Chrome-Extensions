70618a85d6d4fd66197431c25ea67242eb0aa301
yeshwanth <yeshwanthp@microsoft.com>
2024-05-14 00:13:33 +0530

    Added notification for duplicate addition

--- MustWatchMoviesChromeExtension/eventPage.js
+++ MustWatchMoviesChromeExtension/eventPage.js
@@ -1,16 +1,36 @@
-var contextMenuItem = {
-    "id": "movieList",
-    "title": "Add Movie to the List",
-    "contexts": ["selection"]
-};
+chrome.runtime.onInstalled.addListener(async () => {
+    var contextMenuItem = {
+        "id": "movieList",
+        "title": "Add Movie to the List",
+        "contexts": ["selection"]
+    };
 
-chrome.contextMenus.create(contextMenuItem);
+    chrome.contextMenus.create(contextMenuItem);
+});
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
     if(clickData.menuItemId == "movieList" && clickData.selectionText){
         chrome.storage.sync.get(['myMovieList'], function(result){
             var newList = result.myMovieList || [];
+            
+            if(newList.includes(clickData.selectionText)){
+                var notifOptions = {
+                    type: 'basic',
+                    iconUrl: 'movie.png',
+                    title: 'Movie was already added!',
+                    message: "The selected movie - " + clickData.selectionText + " was already added to the list"
+                };
 
+                chrome.notifications.create('duplicateMovieNotif', notifOptions, function(notificationId) {
+                    if (chrome.runtime.lastError) {
+                        console.error('Error creating notification:', chrome.runtime.lastError.message);
+                        return;
+                    }
+                    console.log('Notification created:', notificationId);
+                });
+                return;
+            }
+            
             newList.push(clickData.selectionText);
 
             chrome.storage.sync.set({'myMovieList': newList}, function(){
